9c01d85f31571290d93367194b0b302c0284407b
Giacomo Pessotto <giacomopesso@gmail.com>
2023-01-29 20:10:35 +0000

    fixed api response and prompt params

--- routes/index.js
+++ routes/index.js
@@ -31,16 +31,24 @@ router.get('/', cache('2 minutes'), async (req,res) => {
             model: 'code-davinci-002',
             prompt: `${prompt}`,
             temperature: 0.0,
-            max_tokens: 100,
+            max_tokens: 300,
             top_p: 1.0,
             frequency_penalty: 0.0,
             presence_penalty: 0.0,
+            stop: "#",
           })
           const data = apiRes.data.choices[0].text
+          let lines = data.split('\n');
 
-          html  = hljs.highlight(data, {language:`${language}`}).value
+          let i = 0;
+          while (lines[i] === " "){
+            lines.splice(i, 1)
+            i++
+          }
+          let newtext = lines.join("\n")
+          html  = hljs.highlight(newtext, {language:`${language}`}).value
           res.header("Access-Control-Allow-Origin", "*");
-          res.status(200).json([decodeURIComponent(data), html])
+          res.status(200).json([decodeURIComponent(newtext), html])
 
     } catch (error) {
         res.status(500).json({ error })
